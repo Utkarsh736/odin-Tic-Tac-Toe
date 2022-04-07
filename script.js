3fe6f61dde09f91f870ab45376c4d61389db33cd
utkarsh736 <utkarshtomar736@gmail.com>
2022-04-07 19:10:11 +0530

    Empty modules added

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const gameboard = (() => {
-    const a = 'hello';
+    const add = (a,b)=>a+b;
 
-    return {a};
+    return {add};
 })();
 
-gameboard.a;
+const game = (() => {
+    const add = (a,b)=>a+b;
+    return{add};
+})();
+
+
+
+console.log(`gameboard ${gameboard.add(3,5)}`);
+console.log(`game: ${game.add(4,2)}`)
